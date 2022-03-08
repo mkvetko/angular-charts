ae47b5dfcfcd537af0f0618e26f7fdabd7692c59
Martin Kvetko <kvetko.martin@gmail.com>
2022-03-08 23:04:13 +0100

    restructuring project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,21 @@ import { AppComponent } from './app.component';
 import {GoogleChartsModule} from "angular-google-charts";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from "@angular/material/tabs";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavigationComponent,
+    GraphsComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +31,13 @@ PlotlyModule.plotlyjs = PlotlyJS;
     BrowserAnimationsModule,
     MatTabsModule,
     NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')})
+      echarts: () => import('echarts')
+    }),
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
